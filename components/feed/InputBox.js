750d6912cbf87b43a1397b74d5d2d22fa2c7971c
imanolrtega <imanolrtega@gmail.com>
2021-07-08 18:33:30 -0300

    setup firebase db and storage

--- components/feed/InputBox.js
+++ components/feed/InputBox.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/dist/client/image";
 import { useSession } from "next-auth/client";
 import { PhotographIcon, VideoCameraIcon } from "@heroicons/react/solid";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { db } from "../../firebase";
+import firebase from "firebase";
 
 const InputBox = () => {
   const [session] = useSession();
-  const sendPost = () => {};
+  const sendPost = (e) => {
+    e.preventDefault();
+    if (!inputRef.current.value) return; //does nothing;
+    db.collection("post").add({
+      message: inputRef.current.value,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    inputRef.current.value = "";
+  };
+  const inputRef = useRef(null);
 
   return (
     <div className=" bg-white mt-6 rounded-lg shadow font-medium ">
-      <div className="flex space-x-4 p-4 items-center flex-1 ">
+      <div className="flex space-x-4 p-4 items-center flex-1 focus:outline-none">
         <Image
           className="rounded-full"
           alt="profile"
@@ -21,6 +36,7 @@ const InputBox = () => {
         />
         <form className="flex flex-1">
           <input
+            ref={inputRef}
             placeholder={`¿Qué estás pensando, ${session.user.name}?`}
             className="bg-gray-100 items-center outline-none placeholder-gray-500 px-5 rounded-full h-12 flex flex-grow"
           />
@@ -30,15 +46,15 @@ const InputBox = () => {
         </button>
       </div>
       <div className="flex items-center justify-center border-t-2 mx-4">
-        <div className="flex items-center space-x-2 px-10 py-2 hover:bg-gray-100 rounded-lg my-2 cursor-pointer">
+        <div className="iconInput">
           <VideoCameraIcon className="w-8 h-8 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Video en vivo</p>
         </div>
-        <div className="flex items-center space-x-2 px-10  py-2 hover:bg-gray-100 rounded-lg my-2 cursor-pointer">
+        <div className="iconInput">
           <PhotographIcon className="w-8 h-8 text-green-500" />
           <p className="text-xs sm:text-sm xl:text-base">Foto/video</p>
         </div>
-        <div className=" items-center space-x-2 px-10 py-2 hover:bg-gray-100 rounded-lg my-2 cursor-pointer hidden lg:flex">
+        <div className=" items-center space-x-1 flex-grow justify-center p-2 hover:bg-gray-100 rounded-lg my-2 cursor-pointer hidden lg:flex">
           <EmojiHappyIcon className="w-8 h-8 text-yellow-300" />
           <p className="text-xs sm:text-sm xl:text-base">
             Sentimiendo/actividad
---
+++ firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD54iA5Y24A8DVVmwYVhC3qLsr0IWcaeT8",
+  authDomain: "fb-clone-44f9f.firebaseapp.com",
+  projectId: "fb-clone-44f9f",
+  storageBucket: "fb-clone-44f9f.appspot.com",
+  messagingSenderId: "374289387177",
+  appId: "1:374289387177:web:c1b8370bae50ffb59b63b8",
+  measurementId: "G-QE51GMSH6H",
+};
+// Initialize Firebase
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+const db = app.firestore();
+const storage = firebase.storage;
+
+export { db, storage };
